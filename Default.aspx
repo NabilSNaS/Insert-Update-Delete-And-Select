d6ec467f3ca5d39764dbb5fbd0309c024bbbc324
Nabila <hafija.pstu.cse@gmail.com>
2022-07-07 23:53:51 +0600

    first commit

---
+++ Default.aspx
@@ -0,0 +1,50 @@
+﻿ <%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" EnableEventValidation="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+
+<body> 
+   <form id="frm" runat="server">
+       
+        
+        <div>
+        <table>
+            <tr>
+                <td> Firstname </td>
+                <td><asp:TextBox ID="firstname" runat="server"> </asp:TextBox></td>
+            </tr>
+            <tr>
+                <td> Lastname </td>
+                <td><asp:textbox ID="lastname" runat="server"></asp:textbox></td>
+            </tr>
+            <tr>
+                <td> City </td>
+                <td><asp:textbox ID="city" runat="server"></asp:textbox></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center">
+                 <asp:Button ID="Button1" runat="server" Text=" Insert" OnClick="Button1_Click" />
+                 <asp:Button ID="Button2" runat="server" Text="Update" OnClick="Button2_Click" />
+                 <asp:Button ID="Button3" runat="server" Text="Delete" OnClick="Button3_Click" />
+                 <asp:Button ID="Button4" runat="server" Text="View" OnClick="Button4_Click" />
+                    <%--</td>--%>
+            </tr>
+            <tr>
+                <td> Enter ID for Update </td>
+                <td><asp:textbox ID="oldid" runat="server"></asp:textbox></td>
+            </tr>
+            
+        </table>
+        <br />
+
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
+
---
+++ Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\db1.mdf;Integrated Security=True");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (con.State == ConnectionState.Open)
+        {
+            con.Close();
+        }
+        con.Open();
+        disp_data();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "insert into Table2 values('"+firstname.Text+"','"+lastname.Text+"','"+city.Text+"')";
+        cmd.ExecuteNonQuery();
+        firstname.Text = "";
+        lastname.Text = "";
+        city.Text = "";
+
+        disp_data();
+    }
+
+    public void disp_data(){
+     
+
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "select * from Table2";
+        cmd.ExecuteNonQuery();
+        DataTable dt = new DataTable();
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        da.Fill(dt);
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "delete from Table2 where firstname = '"+firstname.Text+"'";
+        cmd.ExecuteNonQuery();
+        firstname.Text = "";
+        disp_data();
+        //Response.Write("delete from Table2 where firstname = '" + firstname.Text + "'");
+
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        SqlCommand cmd = con.CreateCommand();
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "Update Table2 set firstname='"+firstname.Text+"',lastname='"+lastname.Text+"',city='"+city.Text+"' where ID = "+Convert.ToInt32(oldid.Text)+"";
+        cmd.ExecuteNonQuery();
+        firstname.Text = "";
+        lastname.Text = "";
+        city.Text = "";
+
+        disp_data();
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        disp_data();
+    }
+}
